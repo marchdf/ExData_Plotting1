8efce596a580c7e509b5417b06a7c99e8e382e88
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-01-09 15:05:03 -0500

    Finished making the second plot.

--- plot1.R
+++ plot1.R
@@ -3,10 +3,12 @@ plot1 <- function() {
     ## Load the dataset using our custom load function
     df <- load_dataset()
 
-    ## Make the plot
+    ## Name of the plot
     png('plot1.png')
+
+    ## Make the histogram
     hist(df$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+
+    ## Save the plot
     dev.off()
 }
-
-
