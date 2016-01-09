9a49fab22e11b61481d52b51c6b1ddbe8b57f126
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-01-09 14:53:03 -0500

    First plot finished.

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
 plot1 <- function() {
 
-    ## Load the dataset
+    ## Load the dataset using our custom load function
     df <- load_dataset()
-    
+
+    ## Make the plot
+    png('plot1.png')
+    hist(df$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+    dev.off()
 }
 
 
